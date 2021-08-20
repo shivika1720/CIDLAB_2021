574d1059086ab8d02139d979320a84f0f07c974f
Shivika Kejriwal <shivika.kejriwal2000@gmail.com>
2021-08-21 01:01:52 +0530

    second commit

--- src/MyClass.java
+++ src/MyClass.java
@@ -1,9 +1,11 @@
  
-public class MyClass {
+public class MyClass
+{
 
-	public static void main(String[] args) {
-	System.out.println("Hello, we're learning jenkins");
-
+	public static void main(String[] args)
+	{
+	   System.out.println("Hello, we're learning jenkins");
+	   System.out.println("Configuring jenkins");  
 	}
 
 }
