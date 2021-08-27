af67dc356d5dfa0c08841e6e148d7d97d69baafc
Shivika Kejriwal <59910256+shivika1720@users.noreply.github.com>
2021-08-27 18:55:15 +0530

    updated

--- src/main/java/cidlab2/MyClass2.java
+++ src/main/java/cidlab2/MyClass2.java
@@ -5,7 +5,7 @@ public class MyClass2 {
 	public static void main(String[] args) {
 		System.out.println("Hello Jenkins!");
 		System.out.println("Integrating maven, git and jenkins");
-
+		System.out.println("poll scm, 5 stars with space");
 	}
 
 }
